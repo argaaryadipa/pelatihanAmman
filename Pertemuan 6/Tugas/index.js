dc1e228fa4c83eace64872bcbbad0d2d300f3b5f
argaaryadipa <argaaryadipa10@gmail.com>
2023-09-12 22:42:47 +0800

    Tugas Pertemuan 7

--- Pertemuan 6/Tugas/index.js
+++ Pertemuan 6/Tugas/index.js
@@ -1,18 +1,18 @@
 // variable let
-// for (let i = 1; i < 6; i++) {
-//   console.log(i);
-// }
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+}
 
-// for (let i = 1; i < 6; i++) {
-//   console.log(i);
-// }
-// console.log(i);
+for (let i = 1; i < 6; i++) {
+  console.log(i);
+}
+console.log(i);
 
 // variable var
-// for (var i = 1; i < 6; i++) {
-//   console.log(i);
-// }
-// console.log(i);
+for (var i = 1; i < 6; i++) {
+  console.log(i);
+}
+console.log(i);
 
 // menggunakan const
 // const karakter = "Luffy";
