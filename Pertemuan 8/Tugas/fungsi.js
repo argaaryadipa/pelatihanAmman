900e068f2a410d0c0bfe70710e27465ad0b9adde
argaaryadipa <argaaryadipa10@gmail.com>
2023-09-17 23:28:46 +0800

    Update File ZIP Pertemuan 8

--- Pertemuan 8/Tugas/fungsi.js
+++ Pertemuan 8/Tugas/fungsi.js
@@ -3,15 +3,19 @@ var dataBarang = ["Laptop", "Mouse", "Keyboard"];
 function showBarang() {
   var listBarang = document.getElementById("list-barang");
   // clear list barang
-  listBarang.innerHTML = "";
+  // listBarang.innerHTML = "";
+  listBarang.innerHTML =
+    "<tr><th>No</th><th>Nama Barang</th><th>Action</th></tr>";
 
   // cetak semua barang
   for (let i = 0; i < dataBarang.length; i++) {
     var btnEdit = "<a href='#' onclick='editBarang(" + i + ")'>Edit</a>";
     var btnHapus = "<a href='#' onclick='deleteBarang(" + i + ")'>Hapus</a>";
 
-    listBarang.innerHTML +=
-      "<li>" + dataBarang[i] + " [" + btnEdit + " | " + btnHapus + "] </li>";
+    // listBarang.innerHTML +=
+    //   "<li>" + dataBarang[i] + " [" + btnEdit + " | " + btnHapus + "] </li>";
+    listBarang.innerHTML += "<tr><td>" + dataBarang[i] + "</td><td>";
+    " [" + "</td><td>" + btnEdit + " | " + btnHapus + "] " + "</tr>";
   }
 }
 
---
+++ Pertemuan 8/Tugas/index.js
@@ -0,0 +1,7 @@
+let kata = "";
+
+function tampil() {
+  for (let i = 0; i < kata.length; i++) {
+    document.write("<p> Tulisan " + kata + "Perulangan ke " + "</p><br>");
+  }
+}
